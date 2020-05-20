f1880ee8079b26f3f7d6ced75fec186156d1637f
root <ddphillyfan@gmail.com>
2020-05-20 16:54:10 -0400

    Initialized project task

---
+++ src/main/java/com/diego/projectboard/repository/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.diego.projectboard.repository;
+
+import com.diego.projectboard.model.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
